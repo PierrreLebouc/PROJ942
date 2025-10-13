5d097e63492291d29f06cf65c5b63b68c02ca727
PierrreLebouc <pierre.lebouc@etik.com>
2025-10-13 17:07:49 +0200

    new version with IA

--- Crop_Face_model.py
+++ Crop_Face_model.py
@@ -1,37 +1,54 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import cv2
+import requests
 from matplotlib import pyplot as plt
 
 
 # reading an image
-im = cv2.imread('Bill_Gates.jpg')
-height, width, depth = im.shape
+url = "https://raw.githubusercontent.com/PierrreLebouc/PROJ942/main/Bill_Gates.jpg"
+response = requests.get(url)
+image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+#im = cv2.imread(img)
+
+height, width, depth = img.shape
 print(height, width, depth)
-cv2.imshow('original',im)
+cv2.imshow('original',img)
 cv2.waitKey(0)
 
-# Filtering example
-kernel = np.ones((5,5),np.float32)/25
-im_filt = cv2.filter2D(im,-1,kernel)
-cv2.imshow('filtered',im_filt)
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_image.shape
+cv2.imshow('grey',gray_image)
 cv2.waitKey(0)
 
-# Crop example
-#
-im_cropped = im[20:120, 40:160]
-cv2.imshow('cropped image',im_cropped)
+#load the pre-trained classifier
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+#perform the classifier
+face = face_classifier.detectMultiScale(
+    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+
+x1 = face[0][0] - 10
+y1 = face[0][1] - 10
+x2 = face[0][0] + face[0][2] + 10
+y2 = face[0][1] + face[0][3] + 10
+
+cropped_image = gray_image[y1:y2, x1:x2]
+cv2.imshow('cropped',cropped_image)
 cv2.waitKey(0)
 
-# Profile plot example
-x0 = width//2
-profil_vert = im[:,x0,1]
-plt.plot(profil_vert)
-plt.ylabel('vertical profile')
-plt.show()
-im[:,x0] = [0,0,255]
-cv2.imshow('image with line',im)
+
+
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+    
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow('final',img_rgb)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
